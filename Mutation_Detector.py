d575c282759f81f718521091410085c0d2c952d0
Christopher Kyle Horton <christhehorton@gmail.com>
2014-09-25 17:22:37 -0400

    Add main program file (no code just yet).

---
+++ Mutation_Detector.py
@@ -0,0 +1,22 @@
+# This file is part of Mutation-Detector.
+# Copyright (C) 2014 Christopher Kyle Horton <chorton@ltu.edu>
+
+# Mutation-Detector is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Mutation-Detector is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Mutation-Detector. If not, see <http://www.gnu.org/licenses/>.
+
+
+# MCS 5603 Intro to Bioinformatics, Fall 2014
+# Christopher Kyle Horton (000516274), chorton@ltu.edu
+# Last modified: 9/25/2014
+
+# TODO: Program code here satisfying requirement 1 of the assignment
