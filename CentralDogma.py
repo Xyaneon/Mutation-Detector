9a34018b192902cb833a575e8caa2a249b55e6bb
Christopher Kyle Horton <christhehorton@gmail.com>
2014-09-25 20:01:06 -0400

    Add a separate file for the Central Dogma conversion stuff.

---
+++ CentralDogma.py
@@ -0,0 +1,36 @@
+# This file is part of Mutation-Detector.
+# Copyright (C) 2014 Christopher Kyle Horton <chorton@ltu.edu>
+
+# Mutation-Detector is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Mutation-Detector is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Mutation-Detector. If not, see <http://www.gnu.org/licenses/>.
+
+
+# MCS 5603 Intro to Bioinformatics, Fall 2014
+# Christopher Kyle Horton (000516274), chorton@ltu.edu
+# Last modified: 9/25/2014
+
+def complement(original):
+    """Creates the complement of the given strand."""
+    # TODO
+
+def translate(rna):
+    """Translates the given RNA sequence."""
+    # TODO
+
+def transcribe(dna):
+    """Translates the given DNA sequence."""
+    # TODO
+
+def reverse(strand):
+    """Reverses the given strand."""
+    # TODO
